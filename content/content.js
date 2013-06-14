cf3152ef2e9bd1e4e3b8b38713ee7df30f0b207f
Denis <denis@sokolov.cc>
2013-06-14 15:17:10 +0200

    Styling

--- content/content.js
+++ content/content.js
@@ -2,43 +2,49 @@
 	'use strict';
 
 	var populateResults = function(result) {
-		var containerClass = chrome.i18n.getMessage('@@extension_id')+'-infobar';
-
-		var bar = $('<div>')
-			.addClass(containerClass);
-
-			$('<button>')
-				.addClass('close')
-				.text(chrome.i18n.getMessage('infobar_close'))
-				.on('click', function(){
-					$(this).closest('.'+containerClass).remove();
-				})
-				.appendTo(bar);
-
-		if (result.bookmarks.length === 1) {
-			var b = result.bookmarks[0];
-			$('<a>')
-				.prop('href', b.url)
-				.text(b.title)
-				.appendTo(bar);
-		} else if (result.bookmarks.length > 1) {
-			$('<a>')
-				.prop('href', 'chrome://bookmarks/#q='+result.query)
-				.text(chrome.i18n.getMessage('infobar_bookmarks_multiple', [
-					result.bookmarks.length,
-					result.query
-				]))
-				.appendTo(bar);
+		if (result.bookmarks.length === 0) {
+			return;
 		}
 
-		bar
+		var bar = $('<div>')
+			.addClass(chrome.i18n.getMessage('@@extension_id')+'-infobar')
 			.on('click', 'a', function(e){
 				e.preventDefault();
 				chrome.extension.sendMessage({
 					navigate: $(this).prop('href')
 				});
+			});
+
+		$('<button>')
+			.addClass('close')
+			.text(chrome.i18n.getMessage('infobar_close'))
+			.on('click', function(){
+				bar.remove();
 			})
-			.appendTo('body');
+			.appendTo(bar);
+
+
+
+		var bookmarks = $('<div>').addClass('bookmarks').appendTo(bar);
+
+		var b = result.bookmarks[0];
+		$('<a>')
+			.prop('href', b.url)
+			.text(b.title)
+			.appendTo(bookmarks);
+
+		if (result.bookmarks.length > 1) {
+			bookmarks.append(chrome.i18n.getMessage('infobar_bookmarks_and')).append(
+				$('<a>')
+					.prop('href', 'chrome://bookmarks/#q='+result.query)
+					.text(chrome.i18n.getMessage('infobar_bookmarks_multiple', [
+						result.bookmarks.length - 1,
+						result.query
+					]))
+			).append('.');
+		}
+
+		bar.appendTo('body');
 	};
 
 
